2ef28b77b95143848ce308b67dba7d1a7e3c6a80
Gadir Novruzov <gnovruzov2018@ada.edu.az>
2021-08-22 15:18:00 +0400

    Discount.Grpc added

---
+++ src/Services/Discount/Discount.Grpc/Entities/Coupon.cs
@@ -0,0 +1,22 @@
+﻿namespace Discount.Grpc.Entities
+{
+    public class Coupon
+    {
+
+        public Coupon()
+        {
+
+        }
+        public Coupon(string productName, int amount, string description)
+        {
+            ProductName = productName;
+            Amount = amount;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
